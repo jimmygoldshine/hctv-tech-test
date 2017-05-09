864a781ce000c49d4c332d0b2b23f32494694ca6
James <jamesdix@cwgsy.net>
2017-05-09 13:42:57 +0100

    add broadcaster uniquesness check to order class

--- lib/order.rb
+++ lib/order.rb
@@ -8,11 +8,24 @@ class Order
   end
 
   def add_item(broadcaster, delivery)
+    check_broadcaster_is_absent_from_shopping_bag(broadcaster)
     (shopping_bag[delivery] ||= []).push broadcaster
+  end
+
+  def pre_discount_total
+
   end
 
   private
 
   attr_writer :shopping_bag
+
+  def check_broadcaster_is_absent_from_shopping_bag(broadcaster)
+    shopping_bag.each_value do |delivery_bag|
+      if delivery_bag.include?(broadcaster)
+        raise "Error: Broadcaster is already in shopping bag"
+      end
+    end
+  end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe Order do
 
+  let(:discovery) { instance_double("Broadcaster", :name => "Discovery") }
+  let(:disney) { instance_double("Broadcaster", :name => "Disney") }
+  let(:standard_delivery) { instance_double("Delivery", :type => "standard", :price => 10.00) }
+  let(:express_delivery) { instance_double("Delivery", :type => "express", :price => 20.00) }
   let(:material) { instance_double("Material", :id => "WNP/SWCL001/010") }
   let(:order) { described_class.new(material) }
 
@@ -11,13 +15,25 @@ describe Order do
 
   describe "#add_item(broadcaster, delivery)" do
 
-    let(:disney) { instance_double("Broadcaster", :name => "Disney") }
-    let(:standard_delivery) { instance_double("Delivery", :type => "standard", :price => 10.00) }
-
-    it "should add broadcaster to correct delivery type in '@shopping_bag'" do
+    before do
       order.add_item(disney, standard_delivery)
+    end
+
+    it "should add broadcaster to correct delivery type in 'shopping_bag'" do
       expect(order.shopping_bag[standard_delivery]).to eq([disney])
     end
+
+    it "should add another broadcaster to correct delivery type in 'shopping_bag'" do
+      order.add_item(discovery, standard_delivery)
+      expect(order.shopping_bag[standard_delivery]).to eq([disney,discovery])
+    end
+
+    it "should raise an error if broadcaster is already in 'shopping_bag'" do
+      expect{ order.add_item(disney, express_delivery) }.to raise_error(RuntimeError, /Error: Broadcaster is already in shopping bag/)
+    end
+  end
+
+  describe "#pre_discount_total" do
   end
 
 end
