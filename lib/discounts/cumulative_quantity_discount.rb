0efc69d55ebe694026d1ff00db98c6affcece089
James <jamesdix@cwgsy.net>
2017-05-11 17:23:14 +0100

    refactored CumulativeQuantityDiscount

--- lib/discounts/cumulative_quantity_discount.rb
+++ lib/discounts/cumulative_quantity_discount.rb
@@ -12,7 +12,7 @@ class CumulativeQuantityDiscount
   def discount_value(shopping_bag)
     discounted_value = 0.00
     shopping_bag.each_pair do |delivery_type, broadcaster_list|
-      if delivery_methods_match?(delivery_type) && broadcaster_list.size >= quantity_threshold
+      if delivery_methods_match?(delivery_type) && enough_broadcasters?(broadcaster_list)
         discounted_value += (broadcaster_list.size * delivery_type.price) * discount
       end
     end
@@ -20,6 +20,10 @@ class CumulativeQuantityDiscount
   end
 
   private
+
+  def enough_broadcasters?(broadcaster_list)
+    broadcaster_list.size >= quantity_threshold
+  end
 
   def delivery_methods_match?(delivery_type_obj)
     delivery_type_obj.type == delivery_method
